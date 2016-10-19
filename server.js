824d125ff4cbb41075b67623ada4cf70d49e9807
rafa ramirez <rafa ramirez>
2016-10-19 18:20:34 -0500

    server texto modular

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ colors.setTheme({
     data: 'grey'
 });
 //obteniendo configuraciones
-var IP = '127.0.0.1'
-var PORT = 3000;
+var config = require('./config/config');
+
+var IP = config.IP;
+var PORT = CONFIG.PORT;
 //creando el server
 var server = http.createServer(function(req, res){
     res.writeHead(
