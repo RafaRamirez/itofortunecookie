e96dc557ab45221e7366a47b9a6e1f31eb4794aa
rafa ramirez <rafa ramirez>
2016-10-19 17:24:14 -0500

    server de texto con nodemon

---
+++ server.js
@@ -0,0 +1,32 @@
+//cargando el modulo http
+var http = require('http');
+//cargando la libreria de colors
+var colors = require('colors');
+//Generando tema
+colors.setTheme({
+    silly: 'Rainbow',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    info: 'green',
+    data: 'grey'
+});
+//obteniendo configuraciones
+var IP = '127.0.0.1'
+var PORT = 3000;
+//creando el server
+var server = http.createServer(function(req, res){
+    res.writeHead(
+        200, 
+        {
+            'Content-Type':'text/plain',
+            'Server':'buho@0.0.0'
+        }
+    );
+    res.write('hola desde el server...');
+    res.end();
+});
+//Poniendo a escuchar al server
+server.listen(PORT, IP, function(){
+    console.log("> server escuchando en ".info + `http//${IP}:${PORT}/...`.info);
+});
