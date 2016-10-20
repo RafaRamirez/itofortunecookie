1828fb43ef64f47dc78e79372568167b5b8398fc
rafa ramirez <rafa ramirez>
2016-10-20 18:10:52 -0500

    server estatico completo

--- server.js
+++ server.js
@@ -1,34 +1,88 @@
-//cargando el modulo http
+// Cargando el modulo http
 var http = require('http');
-//cargando la libreria de colors
+// Cargando libreri path
+var path = require('path');
+// Cargando la libreria colors
 var colors = require('colors');
-//Generando tema
+var fs = require('fs');
+// Generando un tema
 colors.setTheme({
-    silly: 'Rainbow',
-    input: 'grey',
-    verbose: 'cyan',
-    prompt: 'grey',
-    info: 'green',
-    data: 'grey'
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: ['yellow', 'bgWhite'],
+  debug: 'blue',
+  error: 'red',
+  achivement: 'rainbow'
 });
-//obteniendo configuraciones
+// Obteniendo configruaciones
 var config = require('./config/config');
-
 var IP = config.IP;
-var PORT = CONFIG.PORT;
-//creando el server
-var server = http.createServer(function(req, res){
-    res.writeHead(
-        200, 
-        {
-            'Content-Type':'text/plain',
-            'Server':'buho@0.0.0'
+var PORT = config.PORT;
+
+var counter = 0;
+// Creando el server
+var server = http.createServer(function(req,res){
+    var urlPath = req.url;
+    console.log(`> URL solicitada: ${urlPath}`.silly);
+    if(urlPath == '/'){
+        // Genera una ruta hacia el index.html
+        urlPath = path.resolve('./static/index.html');
+    }else{
+        // Genera una ruta dentro de static
+        urlPath = 
+        path.resolve(config.STATIC_PATH + urlPath);        
+    }
+    // Exratyendo la extencion de lo que
+    // vamos a servir
+    var extname = path.extname(urlPath);
+    // Seleccionar el contet-type con base
+    // en el extname
+    var contentType = 'text/plain';
+    switch(extname){
+        case '.html':
+            contentType = 'text/html';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css'
+    }
+    fs.exists(urlPath,function(exists){
+        if(!exists){
+            // No  existe
+            res.writeHead(404,{
+                'Content-Type':'text/html'
+            });
+            res.end('<h1>404 NOT FOUND... :(</h1>');
+        } else{
+            // Si existe
+            // Leemos el archivo y lo servimos
+            fs.readFile(urlPath,function(err, content){
+                if(err){
+                    res.writeHead(500,{
+                        'Content-Type':'text/html'
+                    });
+                    res.end('<h1 style="color: red">500 ERROR</h1>');
+                }else{
+                    // Si pudo leer el archivo
+                    res.writeHead(200,{
+                        'Content-Type':contentType
+                    });
+                    res.end(content);
+                }
+            });
         }
-    );
-    res.write('hola desde el server...');
-    res.end();
+    });
 });
-//Poniendo a escuchar al server
-server.listen(PORT, IP, function(){
-    console.log("> server escuchando en ".info + `http//${IP}:${PORT}/...`.info);
+// Poniendo a escuchar
+// al server
+server.listen(PORT,IP,function(){
+    console.log("> Server escuchando en ".info +
+    `http://${IP}:${PORT}/ ...`.info);
 });
